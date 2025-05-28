ac47caad96c914d66a055b9ded34a4875c39f810
mimy <mimy.nine@proton.me>
2025-05-29 02:11:58 +1000

    Fixed up some formatting

--- build.c
+++ build.c
@@ -34,18 +34,18 @@ char* c_src[] =              // List of all the .c files to be compiled
     "src/main.c",
     // Add in all the .c files here...
 };
-char* c_cflags[] = {         // List of all the flags that will be sent to the 
-#if defined(DEBUG)           // compiler
+char* c_cflags[] = {         // List of all the flags that will be sent to the compiler
+	"-MD",                   // -MD is deeded for header dependance check
+#if defined(DEBUG)
 	"-DDEBUG", "-g",
 #elif defined(RELEASE)
 	"-DRELEASE", "-O3",
 #endif
 	"-Wall",
-	"-MD",                     // Needed for header dependance check
 };
 
 char *build_cc = "clang";          // Compiler to use for building the build.c file
-char* build_c   = "./src/build.c"; // Path to the build.c file
+char* build_c   = "./build.c";     // Path to the build.c file
 char* build_dir = "./out";         // The output directory
 char* build_exe = "./build";       // build.c executable name
 
@@ -123,31 +123,16 @@ void print_help() {
 ({ struct stat __stat; stat(file_path, &__stat); __stat; })
 
 enum Mode { MODE_NONE, MODE_DEV, MODE_REL };
-struct BuildContext {
-    bool lock;
-    time_t last_build;
-    time_t last_modif;
-    enum Mode last_mode;
-};
-struct Config {
-    bool run;
-    bool clean;
-    bool build_only;
-    int run_argc;
-    enum Mode mode;
-};
-struct Path {
-    char dir[PATH_MAX];
-    char base[PATH_MAX];
-};
+struct BuildContext { bool lock; time_t last_build; time_t last_modif; enum Mode last_mode; };
+struct Config { bool run; bool clean; bool build_only; int run_argc; enum Mode mode; };
+struct Path { char dir[PATH_MAX]; char base[PATH_MAX]; };
 
 struct Path split_path(const char *path) {
     struct Path parts = {{0}, {0}};
     char *tmp1 = strdup(path), *tmp2 = strdup(path);
     snprintf(parts.base, sizeof(parts.base), "%s", basename(tmp1));
     snprintf(parts.dir, sizeof(parts.dir), "%s", dirname(tmp2));
-    free(tmp1);
-    free(tmp2);
+    free(tmp1); free(tmp2);
     return parts;
 }
 int exec(const char *format, ...) {
@@ -165,8 +150,7 @@ int exec(const char *format, ...) {
 int rec_mkdir(const char *dir) {
     char tmp[PATH_MAX];
     snprintf(tmp, sizeof(tmp), "%s", dir);
-    for (char *p = tmp + 1; *p; p++)
-        if (*p == '/') { *p = 0; mkdir(tmp, S_IRWXU); *p = '/'; }
+    for (char *p = tmp + 1; *p; p++) if (*p == '/') { *p = 0; mkdir(tmp, S_IRWXU); *p = '/'; }
     return mkdir(tmp, S_IRWXU);
 }
 void append_flags(char *cmd, char **flags, int n) {
@@ -228,8 +212,7 @@ int compile_file(const char *path, struct BuildContext *ctx, bool *changed) {
     if (!*changed) return 0;
     sprintf(output_dir, "%s/%s", build_dir, p.dir);
     struct stat st = {0};
-    if (stat(output_dir, &st) == -1)
-        if (rec_mkdir(output_dir) == -1) printf("Failed %s\n", strerror(errno));
+    if (stat(output_dir, &st) == -1) if (rec_mkdir(output_dir) == -1) printf("Failed %s\n", strerror(errno));
     sprintf(cmd, "%s -c %s -o %s/%s/%s.o ", cc, path, build_dir, p.dir, strtok(p.base, "."));
     append_flags(cmd, c_cflags, ARRAY_SIZE(c_cflags));
     return exec(cmd);
@@ -254,18 +237,12 @@ int main(int argc, char *argv[]) {
             conf.run = true;
             break;
         }
-        if (!strcmp(argv[i], "dev"))
-            conf.mode = MODE_DEV;
-        else if (!strcmp(argv[i], "rel"))
-            conf.mode = MODE_REL;
-        else if (!strcmp(argv[i], "clean"))
-            conf.clean = true;
-        else if (!strcmp(argv[i], "build-only"))
-            conf.build_only = true;
-        else if (!strcmp(argv[i], "help")) {
-            print_help();
-            return 0;
-        } else {
+        if (!strcmp(argv[i], "dev")) conf.mode = MODE_DEV;
+        else if (!strcmp(argv[i], "rel")) conf.mode = MODE_REL;
+        else if (!strcmp(argv[i], "clean")) conf.clean = true;
+        else if (!strcmp(argv[i], "build-only")) conf.build_only = true;
+        else if (!strcmp(argv[i], "help")) { print_help(); return 0; }
+        else {
             printf("[\033[31mERROR\033[0m] Unknown command '%s'\n\n", argv[i]);
             print_help();
             return 1;
@@ -284,31 +261,20 @@ int main(int argc, char *argv[]) {
     }
 
     struct stat st = {0};
-    if (stat(build_dir, &st) == -1) {
-        if (rec_mkdir(build_dir) == -1) { printf("Failed %s\n", strerror(errno)); }
-    }
-
-    if (build_ctx.lock) {
-        printf("[\033[31mERROR\033[0m] Build in progress\n");
-        return 1;
-    } else {
-        build_ctx.lock = true;
-        set_build(&build_ctx, build_file_lock);
-    }
+    if (stat(build_dir, &st) == -1) { if (rec_mkdir(build_dir) == -1) { printf("Failed %s\n", strerror(errno)); } }
+    if (build_ctx.lock) { printf("[\033[31mERROR\033[0m] Build in progress\n"); return 1; }
+    else { build_ctx.lock = true; set_build(&build_ctx, build_file_lock); }
 
     struct stat build_file = STAT_FILE(build_c);
     if ((build_ctx.last_modif < build_file.st_mtim.tv_sec) || build_ctx.last_mode != conf.mode || conf.build_only) {
         int ret;
-        switch (conf.mode) {
-        case MODE_DEV:
+        if (conf.mode == MODE_DEV) {
             build_ctx.last_mode = MODE_DEV;
             if ((ret = exec("%s %s -o %s -DDEBUG -g -Wall", build_cc, build_c, build_exe))) return ret;
-            break;
-        case MODE_REL:
+        } else if (conf.mode == MODE_REL) {
             build_ctx.last_mode = MODE_REL;
-            if ((ret = exec("%s %s -o %s -DRELEASE -Wall", build_cc, build_c, build_exe))) return ret;
-            break;
-        default:
+            if ((ret = exec("%s %s -o %s -DRELEASE -O3 -Wall", build_cc, build_c, build_exe))) return ret;
+        } else {
             build_ctx.last_mode = MODE_NONE;
             if ((ret = exec("%s %s -o %s -Wall", build_cc, build_c, build_exe))) return ret;
         }
@@ -324,13 +290,9 @@ int main(int argc, char *argv[]) {
                 sprintf(cmd, "%s %s", cmd, argv[i]);
             }
             return exec(cmd);
-        } else {
-            return 0;
-        }
+        } else return 0;
     }
-
     for (int i = 0; i < sizeof(pre_build) / sizeof(pre_build[0]); i++) exec(pre_build[i]);
-
     bool change = false, file_changed = false;
     for (int i = 0; i < (sizeof(c_src) / sizeof(c_src[0])); i++) {
         int ret = compile_file(c_src[i], &build_ctx, &file_changed);
@@ -342,30 +304,24 @@ int main(int argc, char *argv[]) {
         }
         change = change || file_changed;
     }
-    if (change) {
-        if (!compile_exe()) {
-            printf("[\033[31mERROR\033[0m] Failed to build exe\n");
-            build_ctx.lock = false;
-            set_build(&build_ctx, build_file_lock);
-            return 2;
-        }
-    } else printf("[\033[36mINF\033[0m] Nothing to recompile\n");
+    if (change && !compile_exe()) {
+        printf("[\033[31mERROR\033[0m] Failed to build exe\n");
+        build_ctx.lock = false;
+        set_build(&build_ctx, build_file_lock);
+        return 2;
+    }
     build_ctx.last_build = time(NULL);
     build_ctx.lock = false;
     set_build(&build_ctx, build_file_lock);
-
     for (int i = 0; i < sizeof(post_build) / sizeof(post_build[0]); i++) exec(post_build[i]);
-    for (int i = 0; i < sizeof(pre_run) / sizeof(pre_run[0]); i++) exec(pre_run[i]);
-
     int ret = 0;
     if (conf.run) {
+        for (int i = 0; i < sizeof(pre_run) / sizeof(pre_run[0]); i++) exec(pre_run[i]);
         char cmd[PATH_MAX];
         sprintf(cmd, "%s/%s", build_dir, c_exe);
         for (int i = conf.run_argc + 1; i < argc; i++) sprintf(cmd, "%s %s", cmd, argv[i]);
         ret = exec(cmd);
+        for (int i = 0; i < sizeof(post_run) / sizeof(post_run[0]); i++) exec(post_run[i]);
     }
-
-    for (int i = 0; i < sizeof(post_run) / sizeof(post_run[0]); i++) exec(post_run[i]);
-
     return ret;
 }
