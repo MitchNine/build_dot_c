62b56b4883976ca648f9acaec337fc33e09f2edb
mimy <mimy.nine@proton.me>
2025-06-27 20:53:17 +1000

    Add sig

---
+++ examples/hello_world/src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("This is a placeholder for the main application logic.\n");
+    printf("You can add your application code here.\n");
+    printf("For now, it just prints this message.\n");
+    return 0;
+}
